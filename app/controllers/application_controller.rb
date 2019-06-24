0c07e20b9d0b9405b27854d64f47dec325fc952c
Krishna Krishna <kvanst3@gmail.com>
2019-06-24 19:00:58 +0200

    set up devise for login management and implemented dirty navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  # helper_method :resource_name, :resource, :devise_mapping, :resource_class
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @movies = Movie.all
   end
+
+
 end
