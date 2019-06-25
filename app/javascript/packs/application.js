e8e4199cd0fc4b57a32d388700a29ff0dbf2a834
Krishna Krishna <kvanst3@gmail.com>
2019-06-25 12:58:52 +0200

    Users can now store movies in their library

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,10 @@
 import "bootstrap";
+
+require('dotenv').config();
+
 // import { fetchMovies } from './omdb';
 
-// console.log('Hello World from Webpacker')
 
-//   const form = document.getElementById("keyword");
-//   const search = document.getElementById("search-button")
-
-//   search.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     console.log('Hello World from Webpacker')
-// });
 const list = document.querySelector('#results');
 const form = document.querySelector('#search-movies');
 const query = document.getElementById("keyword");
@@ -21,7 +16,7 @@ const insertMovies = (data) => {
       <img class="search-img" src="${result.Poster}" alt="" />
       <div id='popup'>Extended info about a user</div>
       <p>${result.Title}</p>
-      <div id="create-button"><a data-method="post" href="/movies?title=[${result.Title}]"> Add to Wishlist </a></div>
+      <div id="create-button"><a data-method="post" href="/movies?id=${result.imdbID}"> Add to Wishlist </a></div>
     </li>`;
     list.insertAdjacentHTML('beforeend', movie);
   });
@@ -30,7 +25,7 @@ const insertMovies = (data) => {
 };
 
 const fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=b4f8e5fe`)
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=${process.env['OMDB_KEY']}`)
     .then(response => response.json())
     .then(insertMovies);
 };
